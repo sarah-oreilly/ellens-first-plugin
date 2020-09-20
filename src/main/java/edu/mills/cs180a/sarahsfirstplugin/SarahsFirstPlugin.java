15d5bd307840cee32bb6f7a3d5fce719560af73c
Sarah O'Reilly <soreilly@mills.edu>
2020-09-19 17:19:39 -0700

    Add spawn with cookies to plugin

--- src/main/java/edu/mills/cs180a/sarahsfirstplugin/SarahsFirstPlugin.java
+++ src/main/java/edu/mills/cs180a/sarahsfirstplugin/SarahsFirstPlugin.java
@@ -1,10 +1,18 @@
 package edu.mills.cs180a.sarahsfirstplugin;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SarahsFirstPlugin extends JavaPlugin {
+public class SarahsFirstPlugin extends JavaPlugin implements Listener {
   @Override
   public void onEnable() {
+    getServer().getPluginManager().registerEvents(this, this);
     getLogger().info("SarahsFirstPlugin.onEnable has been invoked!");
   }
 
@@ -12,4 +20,16 @@ public class SarahsFirstPlugin extends JavaPlugin {
   public void onDisable() {
     getLogger().info("SarahsFirstPlugin.onDisable has been invoked!");
   }
+
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent event) {
+    Player player = event.getPlayer();
+    PlayerInventory inventory = player.getInventory();
+    ItemStack itemstack = new ItemStack(Material.COOKIE, 5);
+
+    inventory.addItem(itemstack);
+    player.sendMessage("Every good adventure needs a good snack! We baked you some cookies.");
+
+  }
+
 }
